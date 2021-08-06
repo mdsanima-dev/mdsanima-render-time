5d5588dd8fc3391464f2ef09817064b65993c64c
mdsanima <marcinrozewski@gmail.com>
2021-08-06 23:28:43 +0200

    fix: addon ``bl_info`` dictionary filled
    Support ``TESTING`` for now. In the future it will be changed
    on the ``COMMUNITY`` addon. Links may change.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "MDSANIMA RenderTime",
+    "description": "Estimated total rendering time for all frames",
+    "author": "Marcin Różewski (mdsanima)",
+    "license": "GPL",
+    "deps": "",
+    "version": (0, 1, 0),
+    "blender": (2, 93, 1),
+    "location": "View3D > UI",
+    "warning": "",
+    "doc_url": "https://github.com/mdsanima-dev/mdsanima-render-time/",
+    "wiki_url": "https://github.com/mdsanima-dev/mdsanima-render-time/wiki",
+    "tracker_url": "https://github.com/mdsanima-dev/mdsanima-render-time/issues",
+    "link": "https://mdsanima.com",
+    "support": "TESTING",
+    "category": "Render"
+    }
