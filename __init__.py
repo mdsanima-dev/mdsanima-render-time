5a5c64d3398dec3bf6a7d102e3f73c170a8b5649
mdsanima <marcinrozewski@gmail.com>
2021-08-07 15:47:04 +0200

    feat: first simple layout text and icon

--- __init__.py
+++ __init__.py
@@ -51,18 +51,71 @@ class MDSRT_PT_render_time(bpy.types.Panel):
     bl_category = "MDSANIMA RenderTime"
 
     def draw(self, context):
+        # initial variable layout
+        layout = self.layout
+        scene = bpy.context.scene
+
         # initial variable get icons from dictionary
         pcoll = preview_collections["main"]
         ico_w = pcoll["ic_mds_whit"]
         ico_b = pcoll["ic_mds_blue"]
 
-        # initial variable layout
-        layout = self.layout
+        # initial variable calculation resolution
+        perc_res_v = 100 / scene.render.resolution_percentage
+        res_x = scene.render.resolution_x / perc_res_v
+        res_y = scene.render.resolution_y / perc_res_v
+        res_val = str(int(res_x)) + " x " + str(int(res_y))
+
+        # initial variable calculation all frames
+        sta_frames = scene.frame_start
+        end_frames = scene.frame_end
+        check_frame = 1 if sta_frames == 1 or sta_frames == 0 else 0
+        all_frames = (end_frames - sta_frames) + check_frame
+        time_code = bpy.utils.smpte_from_frame(all_frames)
+
+
+        # render properties stats info
+        row = layout.row()
+        row.label(text="Render Engine", icon="DESKTOP")
+        row.label(text=scene.render.engine)
+
+        row = layout.row()
+        row.label(text="Device", icon="MEMORY")
+        row.label(text=scene.cycles.device)
+
+        row = layout.row()
+        row.label(text="Samples", icon="SHADING_BBOX")
+        row.label(text=str(scene.cycles.samples))
+
 
         layout.separator()
 
+
+        # output properties stats info
         row = layout.row()
-        row.operator("render.render", text="Render", icon_value=ico_w.icon_id)
+        row.label(text="Resolution", icon="RESTRICT_VIEW_ON")
+        row.label(text=res_val)
+
+        row = layout.row()
+        row.label(text="All Frames", icon="RENDER_ANIMATION")
+        row.label(text=str(all_frames))
+
+        row = layout.row()
+        row.label(text="Frame Rate", icon="OUTLINER_DATA_CAMERA")
+        row.label(text=str(scene.render.fps) + " fps")
+
+        row = layout.row()
+        row.label(text="Duration TC", icon="PREVIEW_RANGE")
+        row.label(text=time_code)
+
+
+        layout.separator()
+
+
+        # render button operator
+        row = layout.row()
+        row.operator("render.render", text="Render", icon_value=ico_b.icon_id)
+
 
         layout.separator()
 
