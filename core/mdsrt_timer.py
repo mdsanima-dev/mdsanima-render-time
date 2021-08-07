faa03256a6d99ef86558389b9e349e424485981a
mdsanima <marcinrozewski@gmail.com>
2021-08-07 03:55:37 +0200

    fix: added variable and return time for all ``core`` function

--- core/mdsrt_timer.py
+++ core/mdsrt_timer.py
@@ -1,23 +1,34 @@
 """
 Module contains functions to printing render time in the console.
+Functions returning the date and time.
 """
 
 
 from datetime import datetime
 
 
+rt_info = "[MDSANIMA RT]"
+
+
 def rt_init(scene):
-    print("[MDSANIMA RT] ->", datetime.now(), "Render Init")
+    time_init = datetime.now()
+    print(rt_info, "->", str(time_init).ljust(26), "=> Render Init")
+    return time_init
 
 
 def rt_start(scene):
-    print("[MDSANIMA RT] ->", datetime.now(), "Render Start")
+    time_start = datetime.now()
+    print(rt_info, "->", str(time_start).ljust(26), "=> Render Start")
+    return time_start
 
 
 def rt_complete(scene):
-    print("[MDSANIMA RT] ->", datetime.now(), "Render Complete")
+    time_complete = datetime.now()
+    print(rt_info, "->", str(time_complete).ljust(26), "=> Render Complete")
+    return time_complete
 
 
 def rt_cancel(scene):
-    print("[MDSANIMA RT] ->", datetime.now(), "Render Cancel")
-
+    time_cancel = datetime.now()
+    print(rt_info, "->", str(time_cancel).ljust(26), "=> Render Cancel")
+    return time_cancel
